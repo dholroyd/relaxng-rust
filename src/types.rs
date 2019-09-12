60c0925c6142b06f726bf6040f18d8e232f4f6ac
David Holroyd <dave@badgers-in-foil.co.uk>
2019-09-12 07:37:58 +0100

    Utility to get the text of a Literal

--- src/types.rs
+++ src/types.rs
@@ -163,6 +163,16 @@ pub enum NamespaceUriLiteral {
 
 #[derive(Debug, PartialEq)]
 pub struct Literal(pub Vec<LiteralSegment>);
+impl Literal {
+    pub fn as_string_value(&self) -> String {
+        // TODO any unescaping etc reqiored?
+        let mut val = String::new();
+        for s in self.0.iter() {
+            val.push_str(&s.body);
+        }
+        val
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct LiteralSegment {

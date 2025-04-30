1121fd6a695a0da2e741ac055134836fc19463d0
Marc-André Lureau <marcandre.lureau@redhat.com>
2025-04-30 14:38:08 +0400

    model/bench: replace deprecated fn, skip if files are missing
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- relaxng-model/benches/bench.rs
+++ relaxng-model/benches/bench.rs
@@ -3,22 +3,28 @@ use relaxng_model::Compiler;
 use std::path::Path;
 
 fn relaxng_compile(c: &mut Criterion) {
-    c.bench(
-        "compile_many",
-        Benchmark::new("compile_many", move |b| {
-            b.iter(|| {
-                let tests = [
-                    "../relaxng-compact-syntax/resources/examples/fo/main.rnc",
-                    "../../dash-mpd/DASH-MPD.rnc",
-                ];
-                for n in &tests {
-                    let mut compiler = Compiler::default();
-                    let input = Path::new(n);
-                    compiler.compile(input).expect("compiler.load()");
-                }
-            });
-        }),
-    );
+    let mut group = c.benchmark_group("compile_many");
+    let tests = [
+        "../relaxng-compact-syntax/resources/examples/fo/main.rnc",
+        "../../dash-mpd/DASH-MPD.rnc",
+    ];
+
+    for test in &tests {
+        if !Path::new(test).exists() {
+            println!("Missing file: {}", test);
+            return;
+        }
+    }
+    group.bench_function("compile_many", |b| {
+        b.iter(|| {
+            for n in &tests {
+                let mut compiler = Compiler::default();
+                let input = Path::new(n);
+                compiler.compile(input).expect("compiler.load()");
+            }
+        });
+    });
+    group.finish();
 }
 
 criterion_group!(benches, relaxng_compile);
